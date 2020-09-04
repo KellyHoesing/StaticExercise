301215b7bf78a20899b9b66ca1229eeea4f0945a
Kelly <kelly.hoesing@gmail.com>
2020-09-04 10:57:16 -0500

    Initial Setup

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"What is your Temperature in Celsius?");
+            double celsiusInput = double.Parse(Console.ReadLine());
+            TempConverter.CelsiusToFarenheit(celsiusInput);
+            Console.WriteLine($"Your temperature is {TempConverter.CelsiusToFarenheit(celsiusInput)} degrees Farenheit");
+
+            Console.WriteLine($"What is your Temperature in Farenheit?");
+            double farenheitInput = double.Parse(Console.ReadLine());
+            TempConverter.FarenheitToCelsius(farenheitInput);
+            Console.WriteLine($"Your temperature is {TempConverter.FarenheitToCelsius(farenheitInput)} degrees Celsius");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        public static double FarenheitToCelsius(double farenheit)
+        {
+            var result = (farenheit - 32) / 1.8;
+            return result;
+        }
+        public static double CelsiusToFarenheit(double celsius)
+        {
+            var result = celsius * 1.8 + 32;
+            return result;
+        }
+
+    }
+}
